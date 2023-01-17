3faef366d830aabab83f3ee9954a112862ab7311
unknown <nedelescuramona@yahoo.com>
2023-01-17 15:51:10 +0200

    new homework

---
+++ homework.js
@@ -0,0 +1,62 @@
+// exercise 1
+let start = 0;
+let end = 20;
+
+
+for (i=start; i<= end; i++) {
+  if (i % 2 == 0){
+    console.log (i);
+  }
+}
+
+// exercise 2
+
+const numbersToSum = [1, -5, 20, -34, 16, 29, 36];
+function sumArray (arr) {
+  
+  //const arr = numbersToSum;
+  let sum = 0;
+  for (i=0 ; i < numbersToSum.length; i += 1){
+    sum += numbersToSum [i];
+  }
+  return sum;
+
+}
+
+console.log (sumArray(numbersToSum))
+
+// exercise 3
+
+const numbersToReverse = [1, -5, 20, -34, 16, 29, 36, 9];
+
+function array (arr) {
+  for (let i = numbersToReverse.length- 1;i >= 0 ; i -=1 ) {
+   console.log ( i, numbersToReverse [i])
+    
+
+  }
+}
+console.log (array (numbersToReverse))
+
+// exercise 4
+
+const input = "string to test";
+
+
+function getCount (str) {
+  let vowelsCount = 0
+  const vowels = [ 'a', 'e', 'i', 'o', 'u' ]
+
+  for (let letter of str) {
+    if (vowels.includes(letter)) {
+      vowelsCount ++;
+    }
+  }
+  return vowelsCount;
+
+
+};
+
+const result = getCount(input);
+
+console.log (result)
